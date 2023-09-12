faf382601b6cde15e26dccd382bb588d384153de
axel <axel.rl.404@gmail.com>
2023-09-12 14:37:55 +0700

    cicd: add env

--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -22,3 +22,38 @@ variable "cloud_run_location" {
   type        = string
   description = "Google cloud run location"
 }
+
+variable "mc_cache_dialect" {
+  type        = string
+  description = "Cache dialect"
+}
+
+variable "mc_cache_address" {
+  type        = string
+  description = "Cache address"
+}
+
+variable "mc_cache_password" {
+  type        = string
+  description = "Cache password"
+}
+
+variable "mc_cache_time" {
+  type        = string
+  description = "Cache time"
+}
+
+variable "mc_log_level" {
+  type        = number
+  description = "Log level"
+}
+
+variable "mc_log_json" {
+  type        = bool
+  description = "Log json"
+}
+
+variable "mc_newrelic_license_key" {
+  type        = string
+  description = "Newrelic license key"
+}
